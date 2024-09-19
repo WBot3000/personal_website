6bf8b355ec55e1d84df5d0602f91a29c80821575
Walker Bove <walkerbove333@gmail.com>
2024-09-18 21:34:39 -0400

    Added statistics to info boxes in Interests.

--- src/app/interests/page.tsx
+++ src/app/interests/page.tsx
@@ -17,18 +17,45 @@ const MY_INTERESTS: InfoBoxData[] = [
     {
         name: "Wario Land/Ware",
         description: "Out of all the Mario characters, Wario's my personal favorite. So the fact that he got not one, but two series of games was pretty epic. I'm personally a big fan of Wario Land 3. As for Warioware, I'm a big fan of Gold. A little bit of everything, it's quite nice.",
+        stats: [{
+            label: "Favorite Character", value: "Wario"
+        },
+        {
+            label: "Favorite Game (Land)", value: "Wario Land 3"
+        },
+        {
+            label: "Favorite Game (Ware)", value: "Warioware Gold"
+        }],
         pictureSrc: WarioPicture,
         pictureAlt: "A picture of Wario, specifically the box art for Wario Land 4."
     },
     {
         name: "Mega Man",
-        description: "Mega Man has been a favorite series of mine since I was a young kid. Mostly the classic series, but I enjoy the X series too. I've always liked Proto Man (hence the picture). I actually made a custom Proto Man costume for Halloween and wore it for like four years. My favorite bosses are Guts Man and Elec Man from the first game.",
+        description: "Mega Man has been a favorite series of mine since I was a young kid. Mostly the classic series, but I enjoy the X series too. I actually made a custom Proto Man costume for Halloween and wore it for like four years. But I still don't know whether it's technically Megaman or Mega Man.",
+        stats: [{
+            label: "Favorite Character", value: "Proto Man"
+        },
+        {
+            label: "Favorite Game", value: "Mega Man 5"
+        },
+        {
+            label: "Favorite Bosses", value: "Guts Man and Elec Man"
+        }],
         pictureSrc: MegamanPicture,
         pictureAlt: "Mega Man and Proto Man alongside the Mega Man 10 Robot Masters."
     },
     {
         name: "It's Always Sunny In Philadelphia",
-        description: "The Gang Appears on a Website. Very funny show. My favorite character here is Frank. Gotta love Danny DeVito.",
+        description: "The Gang Appears on a Website. Very funny show. Gotta love Danny DeVito. There's a bunch of good episodes, but here's two of them.",
+        stats: [{
+            label: "Favorite Character", value: "Frank Reynolds"
+        },
+        {
+            label: "Favorite Episode (#1)", value: "The Gang Solves the Gas Crisis"
+        },
+        {
+            label: "Favorite Episode (#2)", value: "The Gang Tries Desperately to Win an Award"
+        }],
         pictureSrc: SunnyPicture,
         pictureAlt: "Dennis, Mac, Charlie, Dee, and Frank all sitting together."
     }
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,7 +1,7 @@
 import { Statistic } from "@/definitions/types";
 
 export default function Statistics({ statistics }: {statistics: Statistic[]}) {
-    return <table className="table-auto border-collapse border border-black">
+    return <table className="w-full table-auto border-collapse border border-black">
         <tbody>
             {statistics.map(stat => <StatisticItem key={stat.label + stat.value} label={stat.label} value={stat.value}/>)}
         </tbody>
