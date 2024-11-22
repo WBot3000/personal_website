000f11668b78bb34fd1085f53bc872ab5b28080c
Walker Bove <walkerbove333@gmail.com>
2024-11-21 20:20:27 -0500

    Added LinkedIn. Logo needs some work though.

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import GithubIcon from "#/link_logos/github.svg";
 import GitlabIcon from "#/link_logos/gitlab.svg";
 import CredlyIcon from "#/link_logos/credly.svg";
+import LinkedInIcon from "#/link_logos/linkedin.svg";
 
 const LINK_DATA: LinkData[] = [
     {
@@ -23,11 +24,17 @@ const LINK_DATA: LinkData[] = [
         link: "https://www.credly.com/users/walker-bove",
         iconSrc: CredlyIcon,
         iconAlt: "Credly Icon"
+    },
+    {
+        label: "LinkedIn®",
+        link: "https://www.linkedin.com/in/walker-bove-79a0991b8/",
+        iconSrc: LinkedInIcon,
+        iconAlt: "LinkedIn Icon"
     }
 ]
 
 export default function Links() {
-    return <section className="lg:w-1/2 w-5/6 mx-auto p-4">
+    return <section className="lg:w-3/4 w-5/6 mx-auto p-4">
         {LINK_DATA.map(data => {
             return <div key={data.link} className="p-4 rounded-sm border-4 border-gray-500 hover:text-emerald-900 text-2xl bg-gradient-to-b hover:from-lime-200 hover:to-green-400 from-cyan-200 to-sky-300 border-collapse">
                 <a href={data.link} key={data.label} className="flex flex-wrap justify-between">
